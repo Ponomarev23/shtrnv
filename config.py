89d39f90a12875cb9d13909da8757c23ca8999e5
shtrnv <44314732+Ponomarev23@users.noreply.github.com>
2025-01-28 06:57:47 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+# Токен вашего Telegram-бота
+TOKEN = '7797487743:AAH5BUOV-SY0Wuws5-LrArfXt5GjtXQppuI'  # Замените на ваш токен, полученный у @BotFather
+
+# Настройки базы данных
+DATABASE_NAME = 'database.db'  # Имя файла базы данных
